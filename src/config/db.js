260c4a58947812e3eb8e1e6deadf897a726b7f1c
Jonas <jp.marcilio2@gmail.com>
2025-11-28 13:48:24 -0300

    FEAT: creating basic structure

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+   try {
+      await mongoose.connect(process.env.MONGO_URL)
+      console.log('✅ MongoDB: conectado com sucesso!')
+   }
+   catch (error) {
+      console.error('❌ MongoDB: erro ao conectar, ', error.message)
+      process.exit(1)
+   }
+}
+
+module.exports = connectDB
